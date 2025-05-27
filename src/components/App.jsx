6d52246e1223448bbb8f1fef96498c5253726468
Paulo Henrique <paulohenriquenazareno@gmail.com>
2025-05-27 14:27:40 -0300

    refiz as pastas e arquivos

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+export const App = () => {
+  return(
+    <h1>Hello world! xilitoso</h1>
+  )
+}
--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import './Header.css';
-import Logo from '../Logo/Logo';
-export default function Header () {
-  return(
-    <header>
-      <Logo src={'../../../public/Logo.svg'} cor={'#c92071'}/>
-    </header>
-  );
-}
